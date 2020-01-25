e5cf9c7b84dc13b54a2ab21370406302b22d5225
MichaelVL <mvl.gh@network42.dk>
2020-01-25 15:13:45 +0100

    Use docker image

--- make-cert.sh
+++ make-cert.sh
@@ -3,21 +3,22 @@ set -e
 
 CERT_SUBJ='/C=DK/ST=ACMEprov/L=ACMEloc/O=ACMEcompany Bar\/Foo/OU=ACMEorg/CN=*.example.com'
 FILENAME=server
+IMAGE=michaelvl/openssl
 
 echo "**********************"
 echo "Generating private key"
 echo "**********************"
 
-docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/work openssl genrsa -out /work/${FILENAME}.key 2048
+docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/work $IMAGE genrsa -out /work/${FILENAME}.key 2048
 
 echo "**************"
 echo "Generating CSR"
 echo "**************"
 
-docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/work openssl req -subj "$CERT_SUBJ" -new -key ${FILENAME}.key -out /work/${FILENAME}.csr
+docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/work $IMAGE req -subj "$CERT_SUBJ" -new -key ${FILENAME}.key -out /work/${FILENAME}.csr
 
 echo "************"
 echo "CSR content:"
 echo "************"
 
-docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/work:ro openssl req -verify -in /work/${FILENAME}.csr -text -noout
+docker run --rm -it --user $(id -u):$(id -g) -v $(pwd):/work:ro $IMAGE req -verify -in /work/${FILENAME}.csr -text -noout
